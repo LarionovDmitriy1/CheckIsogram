823fbb76014b6f2b0268d1a24372825c172bdc64
Дима Ларионов <dima_larionov_000@mail.ru>
2022-12-15 23:22:53 +0300

    Добавьте файлы проекта.

---
+++ CheckIsogram/Program.cs
@@ -0,0 +1,4 @@
+﻿using CheckIsogram;
+Console.WriteLine("Введите слово для проверки на изограмму");
+var result = TheWorkOfCheckingTheIsogram.CheckingIsogramOrNot(Console.ReadLine());
+Console.WriteLine(result);
---
+++ CheckIsogram/TheWorkOfCheckingTheIsogram.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckIsogram;
+
+public class TheWorkOfCheckingTheIsogram
+{
+    public static string CheckingIsogramOrNot(string enter)
+    {
+        if (!String.IsNullOrEmpty(enter))
+        {
+            if (CheckLetter(enter))
+            {
+                for (int i = 0; i < enter.Length; i++)
+                {
+                    int count = 0;
+                    for (int j = 0; j < enter.Length; j++)
+                    {
+                        if (enter[i] == enter[j])
+                        {
+                            count++;
+                            if (count > 1)
+                            {
+                                return "Слово не изограмма";
+                            }
+                        }
+                    }
+                }
+                return "Слово изограмма";
+            }
+            else
+            {
+                return "Введите корректное значение";
+            }
+        }
+        else
+        {
+            return "Строка пуста";
+        }
+    }
+    private static bool CheckLetter(string word)
+    {
+        foreach (char letter in word)
+        {
+            if (Char.IsLetter(letter))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
